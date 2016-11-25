0a0c3b261e8f7b87d69e7a4348f0452e3f6d84cf
NoobTW <noob@noob.tw>
2016-11-25 22:38:44 +0800

    add insrt, find, remove

--- test.js
+++ test.js
@@ -8,11 +8,28 @@ mc.connect('mongodb://vps.noob.tw:27017/database', (err, db) => {
 
 	var collection = db.collection('collection');
 
-	var data = {
-		name: 'g8man',
-		sex: 'M',
-		age: 'old'
-	};
+	// var data = {
+	// 	name: 'g8man',
+	// 	sex: 'M',
+	// 	age: 'old'
+	// };
+
+	// fs.readFile('./data.json', (err, str) => {
+	// 	if(err){
+	// 		console.log('ERRRORROROROROROOROROROR');
+	// 	}else{
+	// 		var data = JSON.parse(str);
+	// 		console.log(data);
+	// 		// collection.insert(data, (err, result) => {
+	// 		// 	if(!err){
+	// 		// 		console.log(result);
+	// 		// 	}else{
+	// 		// 		console.log(err);
+	// 		// 	}
+	// 		// 	db.close();
+	// 		// });
+	// 	}
+	// });
 
 	// collection.insert(data, (err, result) => {
 	// 	if(err){
@@ -23,12 +40,26 @@ mc.connect('mongodb://vps.noob.tw:27017/database', (err, db) => {
 	// 	db.close();
 	// });
 
-	collection.find({name: 'g8man'}).toArray((err, result) => {
+	collection.find({
+		$or: [
+			{District: '旗津區'},
+			{District: '大寮區'}
+		]
+	}).toArray((err, result) => {
 		if(!err){
-			console.log(result.name);
+			console.log(result);
 		}else{
 			console.log(err);
 		}
 		db.close();
-	})
+	});
+
+	// collection.find({District: '旗津區'}, (err, result) => {
+	// 	if(!err){
+	// 		console.log(result);
+	// 	}else{
+	// 		console.log(err);
+	// 	}
+	// 	db.close();
+	// });
 })

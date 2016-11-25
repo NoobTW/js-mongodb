508d327e5c802d7dc91294256a70a8c226cff622
NoobTW <noob@noob.tw>
2016-11-25 22:30:45 +0800

    mongo first try

--- test.js
+++ test.js
@@ -1,8 +1,34 @@
+var fs = require('fs');
 var mongodb = require('mongodb');
 
-var mongodbServer = new mongodb.Server('vps.noob.tw', 27017, { auto_reconnect: true, poolSize: 10 });
-var db = new mongodb.Db('test', mongodbServer);
+var mc = mongodb.MongoClient;
 
-db.open((err, client) => {
-	
-});
+mc.connect('mongodb://vps.noob.tw:27017/database', (err, db) => {
+	console.log('connected');
+
+	var collection = db.collection('collection');
+
+	var data = {
+		name: 'g8man',
+		sex: 'M',
+		age: 'old'
+	};
+
+	// collection.insert(data, (err, result) => {
+	// 	if(err){
+	// 		console.log(err);
+	// 	}else{
+	// 		console.log(result);
+	// 	}
+	// 	db.close();
+	// });
+
+	collection.find({name: 'g8man'}).toArray((err, result) => {
+		if(!err){
+			console.log(result.name);
+		}else{
+			console.log(err);
+		}
+		db.close();
+	})
+})
